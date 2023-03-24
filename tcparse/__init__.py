121b4e9627e1f173f82241726c1f548d006b5a16
Colton Hicks <github@coltonhicks.com>
2023-03-24 13:29:57 -0700

    Bumped version. Updated CHANGELOG.md. Added __version__ to __init__.py

--- tcparse/__init__.py
+++ tcparse/__init__.py
@@ -1,1 +1,7 @@
+# https://github.com/python-poetry/poetry/pull/2366#issuecomment-652418094
+from importlib import metadata
+
+__version__ = metadata.version(__name__)
+
+
 from .main import *  # noqa
